cfe16548a00ac3585228d11c94d05134cac9bfb3
Tomas Oh <tomasoh@csu.fullerton.edu>
2025-11-20 19:48:21 -0800

    initial commit

---
+++ src/routes/index.tsx
@@ -0,0 +1,13 @@
+import { createFileRoute } from '@tanstack/react-router'
+
+export const Route = createFileRoute('/')({
+  component: LandingPage,
+})
+
+function LandingPage() {
+  return (
+    <div>
+      <h1>Tradespace</h1>
+    </div>
+  )
+}
