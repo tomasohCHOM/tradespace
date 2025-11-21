8e226119073a208efbad6ef2334498cb19a26b2f
Tomas Oh <tomasoh@csu.fullerton.edu>
2025-11-20 20:42:40 -0800

    disable type-aware linting check for config files

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,12 @@
 //  @ts-check
 
-import { tanstackConfig } from '@tanstack/eslint-config'
+import { tanstackConfig } from '@tanstack/eslint-config';
+import tseslint from 'typescript-eslint';
 
-export default [...tanstackConfig]
+export default [
+  ...tanstackConfig,
+  {
+    files: ['eslint.config.js', 'prettier.config.js', 'vite.config.js'],
+    ...tseslint.configs.disableTypeChecked,
+  },
+];
--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,10 +1,10 @@
-import { Outlet, createRootRouteWithContext } from '@tanstack/react-router'
-import { TanStackRouterDevtoolsPanel } from '@tanstack/react-router-devtools'
-import { TanStackDevtools } from '@tanstack/react-devtools'
-import type { QueryClient } from '@tanstack/react-query'
+import { Outlet, createRootRouteWithContext } from '@tanstack/react-router';
+import { TanStackRouterDevtoolsPanel } from '@tanstack/react-router-devtools';
+import { TanStackDevtools } from '@tanstack/react-devtools';
+import type { QueryClient } from '@tanstack/react-query';
 
 interface MyRouterContext {
-  queryClient: QueryClient
+  queryClient: QueryClient;
 }
 
 export const Route = createRootRouteWithContext<MyRouterContext>()({
@@ -24,4 +24,4 @@ export const Route = createRootRouteWithContext<MyRouterContext>()({
       />
     </>
   ),
-})
+});
