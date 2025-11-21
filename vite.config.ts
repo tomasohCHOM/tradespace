ac8646329d912e5e9fd77d80dc4637fe08675926
Tomas Oh <tomasoh@csu.fullerton.edu>
2025-11-20 20:30:34 -0800

    add ci workflow checking for linting, formatting, and building

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
+import { URL, fileURLToPath } from 'node:url'
 import { defineConfig } from 'vite'
 import { devtools } from '@tanstack/devtools-vite'
 import viteReact from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 import { tanstackRouter } from '@tanstack/router-plugin/vite'
-import { fileURLToPath, URL } from 'node:url'
 
 // https://vitejs.dev/config/
 export default defineConfig({
