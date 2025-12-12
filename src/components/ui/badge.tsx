f9450e6789c58cbaf70a98aa1eabdc50ed922a29
whataboutkris <whataboutkris@gmail.com>
2025-12-11 19:29:48 -0800

    updated badge utils to use the correct import

--- src/components/ui/badge.tsx
+++ src/components/ui/badge.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import { Slot } from '@radix-ui/react-slot';
 import { cva } from 'class-variance-authority';
-import { cn } from '../../utils/badgeutils';
+import { cn } from '../../lib/utils';
 import type { VariantProps } from 'class-variance-authority';
 
 const badgeVariants = cva(
