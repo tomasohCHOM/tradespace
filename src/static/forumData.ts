d92f38ead8d13589935b60758d134d5db3c579c6
Bryant Martinez <bryant.e.martinez@outlook.com>
2025-12-15 22:13:27 -0800

    Update

--- src/static/forumData.ts
+++ src/static/forumData.ts
@@ -1,7 +1,7 @@
 import { Timestamp } from 'firebase/firestore';
 import type { ForumPost } from '@/types/forums';
 
-export const mockPosts: ForumPost[] = [
+export const mockPosts: Array<ForumPost> = [
   {
     id: '1',
     title: 'Best practices for pricing vintage items?',
