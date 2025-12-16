22404b3ffd34ce1bae158087ba67901ce26ff0a6
Bryant Martinez <bryant.e.martinez@outlook.com>
2025-12-15 21:50:49 -0800

    Updated to add virtual assistant to user dashboard
    User assistant will follow the user around each page unless they are signed out, which it will not function.
    
    To use locally add API Key sent to discord.

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -2,6 +2,7 @@ import { Outlet, createRootRouteWithContext } from '@tanstack/react-router';
 import { TanStackRouterDevtoolsPanel } from '@tanstack/react-router-devtools';
 import { TanStackDevtools } from '@tanstack/react-devtools';
 import type { QueryClient } from '@tanstack/react-query';
+import VirtualAssistant from '@/components/VirtualAssistant';
 
 interface MyRouterContext {
   queryClient: QueryClient;
@@ -11,6 +12,11 @@ export const Route = createRootRouteWithContext<MyRouterContext>()({
   component: () => (
     <>
       <Outlet />
+
+      {/* Virtual Assistant q */}
+      <VirtualAssistant />
+    
+      {/* TanStack Devtools - bottom-right corner */}
       <TanStackDevtools
         config={{
           position: 'bottom-right',
